04d13ec8a11886b789f96d7b1ae27d2923875d05
PavloLevchenko <teasetrnet@gmail.com>
2022-01-07 20:30:37 +0200

    Добавляет рендер Statistics в цикле

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -7,22 +7,12 @@ const Statistics = props => {
       {props.title ? <h2 className={s.title}>{props.title}</h2> : ''}
 
       <ul className={s.statList}>
-        <li className={s.item} key={props.stats[0]['id']}>
-          <span className={s.label}>{props.stats[0]['label']}</span>
-          <span className={s.percentage}>{props.stats[0]['percentage']}%</span>
-        </li>
-        <li className={s.item} key={props.stats[1]['id']}>
-          <span className={s.label}>{props.stats[1]['label']}</span>
-          <span className={s.percentage}>{props.stats[1]['percentage']}%</span>
-        </li>
-        <li className={s.item} key={props.stats[2]['id']}>
-          <span className={s.label}>{props.stats[2]['label']}</span>
-          <span className={s.percentage}>{props.stats[2]['percentage']}%</span>
-        </li>
-        <li className={s.item} key={props.stats[3]['id']}>
-          <span className={s.label}>{props.stats[3]['label']}</span>
-          <span className={s.percentage}>{props.stats[3]['percentage']}%</span>
-        </li>
+        {props.stats.map(stat => (
+          <li className={s.item} key={stat['id']}>
+            <span className={s.label}>{stat['label']}</span>
+            <span className={s.percentage}>{stat['percentage']}%</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
